19e87c48e9719ea9b15e1810892e3bf5b93c31ce
Aarjeet-1 <arjeetjoshi175@gmail.com>
2023-12-16 12:56:48 +0545

    completed

--- script.js
+++ script.js
@@ -1,3 +1,105 @@
+// const pwEl = document.getElementById("pw");
+// const copyEl = document.getElementById("copy");
+// const lenEl = document.getElementById("len");
+// const upperEl = document.getElementById("upper");
+// const lowerEl = document.getElementById("lower");
+// const numberEl = document.getElementById("number");
+// const symbolEl = document.getElementById("symbol");
+// const generateEl = document.getElementById("generate");
+
+// const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+// const numbers = "0123456789";
+// const symbols = "!@#$%^&*()_+=";
+
+// function getLowercase() {
+//     return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+// }
+
+// function getUppercase() {
+//     return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+// }
+
+// function getNumber() {
+//     return numbers[Math.floor(Math.random() * numbers.length)];
+// }
+
+// function getSymbol() {
+//     return symbols[Math.floor(Math.random() * symbols.length)];
+// }
+
+// function generatePassword() {
+//     const len = lenEl.value;
+
+//     let password = "";
+
+//     if (upperEl.checked) {
+//         password += getUppercase();
+//     }
+
+//     if (lowerEl.checked) {
+//         password += getLowercase();
+//     }
+
+//     if (numberEl.checked) {
+//         password += getNumber();
+//     }
+
+//     if (symbolEl.checked) {
+//         password += getSymbol();
+//     }
+
+//     for (let i = password.length; i < len; i++) {
+//         const x = generateX();
+//         password += x;
+//     }
+
+//     pwEl.innerText = password;
+// }
+
+// function generateX() {
+//     const xs = [];
+//     if (upperEl.checked) {
+//         xs.push(getUppercase());
+//     }
+
+//     if (lowerEl.checked) {
+//         xs.push(getLowercase());
+//     }
+
+//     if (numberEl.checked) {
+//         xs.push(getNumber());
+//     }
+
+//     if (symbolEl.checked) {
+//         xs.push(getSymbol());
+//     }
+
+//     if (xs.length === 0) return "";
+
+//     return xs[Math.floor(Math.random() * xs.length)];
+// }
+
+// generateEl.addEventListener("click", generatePassword);
+
+// copyEl.addEventListener("click", () => {
+//     const textarea = document.createElement("textarea");
+//     const password = pwEl.innerText;
+
+//     if (!password) {
+//         return;
+//     }
+
+//     textarea.value = password;
+//     document.body.appendChild(textarea);
+//     textarea.select();
+//     document.execCommand("copy");
+//     textarea.remove();
+//     alert("Password copied to clipboard");
+// });
+
+
+
 const passwordEl = document.getElementById("pw");
 const copy_pw =document.getElementById("copy");
 const length = document.getElementById("len");
@@ -38,7 +140,7 @@ function password_generate(){
 
     }
     if (lower_case.checked) {
-        password += getLowercase();
+        password += getLowerCase();
     }
 
     if (number_digit.checked) {
@@ -58,10 +160,10 @@ passwordEl.innerHTML=password;
 function generateX(){
     const xs = [];
     if(upper_case.checked){
-        xs.push(getUpperCase);
+        xs.push(getUpperCase());
     }
     if (lower_case.checked) {
-        xs.push(getLowercase());
+        xs.push(getLowerCase());
     }
 
     if (number_digit.checked) {
@@ -78,7 +180,7 @@ function generateX(){
 
 }
 
-generate_password.addEventListener("click",password_generate());
+generate_password.addEventListener("click",password_generate);
 copy_pw.addEventListener("click", () =>{
     const textarea = document.createElement("textarea");
     const password = passwordEl.innerText;
